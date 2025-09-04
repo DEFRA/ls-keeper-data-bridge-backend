4065ce2105b85145759f47e77bf8286b4facd3ec
Mark Gent <Mark.Gent@homesengland.gov.uk>
2025-09-04 16:43:40 +0100

    Added treat warnings as errors. Testing using localhost.

--- tests/KeeperData.Bridge.Tests.Integration/IntegrationTestFixture.cs
+++ tests/KeeperData.Bridge.Tests.Integration/IntegrationTestFixture.cs
@@ -15,8 +15,8 @@ public class IntegrationTestFixture : IDisposable
 
         HttpClient = new HttpClient(_httpClientHandler)
         {
-            BaseAddress = new Uri("http://localhost:8080"),
-            // BaseAddress = new Uri("http://keeperdata-bridge:8080"),
+            // BaseAddress = new Uri("http://localhost:8080"),
+            BaseAddress = new Uri("http://keeperdata-bridge:8080"),
             Timeout = TimeSpan.FromSeconds(30)
         };
         HttpClient.DefaultRequestHeaders.Add("Accept", "application/json");

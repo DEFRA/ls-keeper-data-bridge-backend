9899f0fb17c89304bd19fd3be024a821da255ac6
huss <huss.rasit@defra.gov.uk>
2025-09-12 14:43:34 +0100

    Change to StorageConfiguration

--- src/KeeperData.Infrastructure/Storage/Configuration/StorageConfiguration.cs
+++ src/KeeperData.Infrastructure/Storage/Configuration/StorageConfiguration.cs
@@ -3,5 +3,5 @@ namespace KeeperData.Infrastructure.Storage.Configuration;
 public record StorageConfiguration
 {
     public StorageWithCredentialsConfiguration ExternalStorage { get; init; } = new();
-    public StorageWithCredentialsConfiguration InternalStorage { get; init; } = new();
+    public StorageConfigurationDetails InternalStorage { get; init; } = new();
 }

9d5eda5b5ac61ffb7d5fa6adb3f88ed17546bf07
Mark Gent <Mark.Gent@homesengland.gov.uk>
2025-09-12 14:47:37 +0100

    Minors to docker-compose image and config

--- compose/start-localstack.sh
+++ compose/start-localstack.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+sed -i 's/\r$//' "$0"
+
 export AWS_REGION=eu-west-2
 export AWS_DEFAULT_REGION=eu-west-2
 export AWS_ACCESS_KEY_ID=test
@@ -37,7 +39,6 @@ else
     --endpoint-url=http://localhost:4566
   echo "S3 bucket created: $INTERNAL_BUCKET_NAME"
 fi
-
 
 echo "Bootstrapping SNS setup..."
 

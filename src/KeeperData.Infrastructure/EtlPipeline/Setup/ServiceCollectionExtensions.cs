1ac6f6464033a9f58e05db81c9e1cc74667e29f8
pc-defra <pete.celliers@esynergy.co.uk>
2026-07-28 09:28:17 +0100

    Feature/lkpr 84 add pipeline storage support (#195)
    * add support for storage (via new pipeline)
    
    * add tests
    
    * rename to ...Provider

---
+++ src/KeeperData.Infrastructure/EtlPipeline/Setup/ServiceCollectionExtensions.cs
@@ -0,0 +1,31 @@
+using System.Diagnostics.CodeAnalysis;
+using KeeperData.Core.EtlPipeline.Storage;
+using KeeperData.Infrastructure.EtlPipeline.Storage;
+using KeeperData.Infrastructure.Storage.Configuration;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace KeeperData.Infrastructure.EtlPipeline.Setup;
+
+[ExcludeFromCodeCoverage]
+public static class ServiceCollectionExtensions
+{
+    /// <summary>Registers storage access for the file-based ETL pipeline folders.
+    /// Must be called after AddStorageDependencies, which registers StorageConfiguration
+    /// and the S3 client factory this depends on.</summary>
+    public static IServiceCollection AddEtlPipelineStorage(this IServiceCollection services, IConfiguration configuration)
+    {
+        var storageConfiguration = configuration.GetSection(nameof(StorageConfiguration)).Get<StorageConfiguration>()!;
+
+        if (storageConfiguration.UseFileSystem)
+        {
+            services.AddTransient<IEtlPipelineStorageProvider, FileSystemEtlPipelineStorageProvider>();
+        }
+        else
+        {
+            services.AddTransient<IEtlPipelineStorageProvider, S3EtlPipelineStorageProvider>();
+        }
+
+        return services;
+    }
+}
